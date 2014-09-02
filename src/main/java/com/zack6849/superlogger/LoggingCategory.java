d10b7208e36cd7cc6e7ea05956bca826c762e57a
zack6849 <zcraig29@gmail.com>
2014-09-01 23:13:34 -0400

    MUH FREEDUMS

--- src/main/java/com/zack6849/superlogger/LoggingCategory.java
+++ src/main/java/com/zack6849/superlogger/LoggingCategory.java
@@ -1,8 +1,23 @@
+/*
+* This file is part of SuperLogger.
+*
+* SuperLogger is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
 package com.zack6849.superlogger;
 
-/**
- * Created by zack6849 on 9/1/14.
- */
 public enum LoggingCategory {
     ALL("log.log"),
     CHAT("chat.log"),
